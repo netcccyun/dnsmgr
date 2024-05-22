c1edf905f30c2c3cb67620762fac920e23e4bb9b
net909 <net909@163.com>
2024-05-22 11:17:34 +0800

    优化解析记录分页

--- public/static/js/custom.js
+++ public/static/js/custom.js
@@ -131,3 +131,31 @@ var isMobile = function(){
 	}
 	return false;
 }
+
+function setCookie(name,value,expire = null)
+{
+	var cookie = name + "=" + escape(value);
+	if(expire){
+		var exp = new Date();
+		exp.setTime(exp.getTime() + expire*1000);
+		cookie += ";expires=" + exp.toGMTString();
+	}
+	document.cookie = cookie;
+}
+function getCookie(name)
+{
+	var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+	if(arr=document.cookie.match(reg))
+		return unescape(arr[2]);
+	else
+		return null;
+}
+function delCookie(name)
+{
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval=getCookie(name);
+    if(cval!=null){
+      document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+    }
+}
