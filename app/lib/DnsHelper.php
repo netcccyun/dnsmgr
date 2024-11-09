b92dcedd510872f1da564d26f16537296b8bcfcb
coolxitech <admin@sorpack.cn>
2024-11-09 12:58:16 +0800

    style(DnsHelper): 为 DNS 帮助器配置添加缺失的逗号
    - 在多个 DNS 服务提供商的配置中添加了缺失的逗号
    - 修复了代码格式不一致的问题，提高了代码的可读性

--- app/lib/DnsHelper.php
+++ app/lib/DnsHelper.php
@@ -11,7 +11,7 @@ class DnsHelper
             'name' => '阿里云',
             'config' => [
                 'ak' => 'AccessKeyId',
-                'sk' => 'AccessKeySecret'
+                'sk' => 'AccessKeySecret',
             ],
             'remark' => 1, //是否支持备注，1单独设置备注，2和记录一起设置
             'status' => true, //是否支持启用暂停
@@ -23,7 +23,7 @@ class DnsHelper
             'name' => '腾讯云',
             'config' => [
                 'ak' => 'SecretId',
-                'sk' => 'SecretKey'
+                'sk' => 'SecretKey',
             ],
             'remark' => 1,
             'status' => true,
@@ -35,7 +35,7 @@ class DnsHelper
             'name' => '华为云',
             'config' => [
                 'ak' => 'AccessKeyId',
-                'sk' => 'SecretAccessKey'
+                'sk' => 'SecretAccessKey',
             ],
             'remark' => 2,
             'status' => true,
@@ -47,7 +47,7 @@ class DnsHelper
             'name' => '百度云',
             'config' => [
                 'ak' => 'AccessKey',
-                'sk' => 'SecretKey'
+                'sk' => 'SecretKey',
             ],
             'remark' => 2,
             'status' => false,
@@ -59,7 +59,7 @@ class DnsHelper
             'name' => '西部数码',
             'config' => [
                 'ak' => '用户名',
-                'sk' => 'API密码'
+                'sk' => 'API密码',
             ],
             'remark' => 0,
             'status' => true,
@@ -71,7 +71,7 @@ class DnsHelper
             'name' => '火山引擎',
             'config' => [
                 'ak' => 'AccessKeyId',
-                'sk' => 'SecretAccessKey'
+                'sk' => 'SecretAccessKey',
             ],
             'remark' => 2,
             'status' => true,
@@ -83,7 +83,7 @@ class DnsHelper
             'name' => 'DNSLA',
             'config' => [
                 'ak' => 'APIID',
-                'sk' => 'API密钥'
+                'sk' => 'API密钥',
             ],
             'remark' => 0,
             'status' => true,
@@ -95,7 +95,7 @@ class DnsHelper
             'name' => 'Cloudflare',
             'config' => [
                 'ak' => '邮箱地址',
-                'sk' => 'API密钥/令牌'
+                'sk' => 'API密钥/令牌',
             ],
             'remark' => 2,
             'status' => false,
