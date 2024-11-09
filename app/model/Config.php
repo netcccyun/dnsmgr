06a0bda77b2dc813192ba8e665ff53fb46eb4e6a
coolxitech <admin@sorpack.cn>
2024-11-09 11:38:40 +0800

    创建数据库模型

---
+++ app/model/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\model;
+
+use think\Model;
+
+class Config extends Model
+{
+}
---
+++ app/model/Dmtask.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\model;
+
+use think\Model;
+
+class Dmtask extends Model
+{
+}
