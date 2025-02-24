0877674efbd000d53528895c8308da8f209e6f39
耗子 <i@haozi.net>
2025-02-24 19:03:06 +0800

    支持括彩云部署

---
+++ app/lib/deploy/kuocai.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\lib\deploy;
+
+use app\lib\DeployInterface;
+use Exception;
+
+class kuocai implements DeployInterface
+{
+    private $logger;
+    private $username;
+    private $password;
+    private $proxy;
+    private $token = null;
+
+    public function __construct($config)
+    {
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->proxy = $config['proxy'] == 1;
+    }
+
+    public function check()
+    {
+        if (empty($this->username) || empty($this->password)) {
+            throw new Exception('请填写控制台账号和密码');
+        }
+        $data = [
+            'userAccount' => $this->username,
+            'userPwd' => $this->password,
+            'remember' => 'true'
+        ];
+        $this->token = $this->request('/login/loginUser', $data);
+    }
+
+    public function deploy($fullchain, $privatekey, $config, &$info)
+    {
+        $id = $config['id'];
+        if (empty($id)) {
+            throw new Exception('域名ID不能为空');
+        }
+        $data = [
+            'doMainId' => $id,
+            'https' => [
+                'certificate_name' => uniqid('cert_'),
+                'certificate_source' => '0',
+                'certificate_value' => $fullchain,
+                'https_status' => 'on',
+                'private_key' => $privatekey,
+            ]
+        ];
+        $this->request('/CdnDomainHttps/httpsConfiguration', $data);
+        $this->log("域名ID:{$id}更新成功！");
+    }
+
+    public function setLogger($func)
+    {
+        $this->logger = $func;
+    }
+
+    private function log($txt)
+    {
+        if ($this->logger) {
+            call_user_func($this->logger, $txt);
+        }
+    }
+
+    private function request($path, $body = null)
+    {
+        $url = 'https://kuocai.cn' . $path;
+        $response = curl_client(
+            $url,
+            $body,
+            null,
+            $this->token ? "kuocai_cdn_token={$this->token}" : null,
+            null,
+            $this->proxy
+        );
+        $result = json_decode($response['body'], true);
+        if (isset($result['code']) && $result['code'] == 'SUCCESS') {
+            return isset($result['data']) ? $result['data'] : null;
+        } elseif (isset($result['message'])) {
+            throw new Exception($result['message']);
+        } else {
+            throw new Exception('请求失败(httpCode=' . $response['code'] . ')');
+        }
+    }
+}
