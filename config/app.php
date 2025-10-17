3499583e0d2075a7574fcca7d8af7456408966bd
net909 <net909@163.com>
2025-10-17 10:41:53 +0800

    version

--- config/app.php
+++ config/app.php
@@ -31,7 +31,7 @@ return [
     'show_error_msg'   => true,
     'exception_tmpl'   => \think\facade\App::getAppPath() . 'view/exception.tpl',
 
-    'version' => '1041',
+    'version' => '1042',
 
     'dbversion' => '1040'
 ];
